f9b0ed9248ec7b2295ee32c8762b84cee7bea094
Muruganandham K <muruganandham23@gmail.com>
2023-06-26 13:12:34 +0530

    Text update

--- Localize-Demo/ViewController.swift
+++ Localize-Demo/ViewController.swift
@@ -8,17 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var countLabel: UILabel!
+
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         displayLabel.text = String(localized: "Good Morning")
         
-        let count = 1
-        countLabel.text = String(localized: "\(count) views")
+//        var count = Int.random(in: 0...10)
+//        var countString = ""
+//        switch count {
+//        case 0:
+//            countString = "Zero points"
+//        case 1:
+//            countString = "1 point"
+//        default:
+//            countString = "\(count) points"
+//        }
+//        countLabel.text = countString
+        
+
+        let count = Int.random(in: 0...10)
+        let string = String(localized: "\(count) points")
+        countLabel.text = string
+        
     }
 }
 
