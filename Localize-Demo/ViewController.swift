9a4bd6c0eaa0a4ba4cedcf5f8351eac21d0b829a
Murugnanandham <muruganandham23@gmail.com>
2024-07-26 16:08:38 +0530

    Fix codacy issues

--- Localize-Demo/ViewController.swift
+++ Localize-Demo/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var displayLabel: UILabel!
     @IBOutlet weak var countLabel: UILabel!
     
@@ -17,23 +17,21 @@ class ViewController: UIViewController {
         
         displayLabel.text = String(localized: "Good Morning")
         
-//        var count = Int.random(in: 0...10)
-//        var countString = ""
-//        switch count {
-//        case 0:
-//            countString = "Zero points"
-//        case 1:
-//            countString = "1 point"
-//        default:
-//            countString = "\(count) points"
-//        }
-//        countLabel.text = countString
+        //        var count = Int.random(in: 0...10)
+        //        var countString = ""
+        //        switch count {
+        //        case 0:
+        //            countString = "Zero points"
+        //        case 1:
+        //            countString = "1 point"
+        //        default:
+        //            countString = "\(count) points"
+        //        }
+        //        countLabel.text = countString
         
-
         let count = Int.random(in: 0...10)
         let string = String(localized: "\(count) points")
         countLabel.text = string
         
     }
 }
-
--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Localizing and varying text with a string catalog in Xcode 15
 
 repo for the following tutorial:
-https://medium.com/@muruganandhamkuppan/ios-localization-in-xcode-15-using-string-catalogs-ff5257a28506
-
+<https://medium.com/@muruganandhamkuppan/ios-localization-in-xcode-15-using-string-catalogs-ff5257a28506>
 reference:
-https://developer.apple.com/documentation/xcode/localizing-and-varying-text-with-a-string-catalog
+<https://developer.apple.com/documentation/xcode/localizing-and-varying-text-with-a-string-catalog>
